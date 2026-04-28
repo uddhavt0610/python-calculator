93d439b4e28c31035a335b6729efcee30164c6fc
Uddhav Tonde <uddhav.tonde0610@gmail.com>
2026-04-28 17:24:43 +0000

    Initial commit - Python calculator with Docker & Kubernetes

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Python Calculator is running!"
+
+@app.route("/add")
+def add():
+    a = int(request.args.get("a", 0))
+    b = int(request.args.get("b", 0))
+    return {"result": a + b}
+
+@app.route("/sub")
+def sub():
+    a = int(request.args.get("a", 0))
+    b = int(request.args.get("b", 0))
+    return {"result": a - b}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
